edba4f2a247065f521e5b4130a1f2584f73bc50e
swasthik-007 <mohanty.swastik4@gmail.com>
2024-05-28 22:43:54 +0530

    Added ui for signup and login page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,9 @@
 import Logo from 'Assets/Images/logo-color.png';
+import { Link } from 'react-router-dom';
 
 export default function Home() {
     return (
             <div className="flex flex-col items-center justify-center gap-20 h-[100vh]">
-
                 <div className="h-48 w-48">
                     <img 
                         className='w-full h-full'
@@ -11,7 +11,6 @@ export default function Home() {
                         src={Logo}
                     />
                 </div>
-
                 <div className='flex justify-around items-center gap-16'>
                     <div className='w-2/4 text-center font-semibold basis-1/2'>
                         <h1 className='text-white text-5xl tracking-widest leading-normal'>
@@ -22,8 +21,12 @@ export default function Home() {
                         </h1>
                     </div>
                     <div>
-                        <button className='btn btn-primary rounded-md px-5 py-2 text-xl'>Register</button>
-                        <button className='btn btn-warning mx-3 rounded-md px-5 py-2 text-xl'>Login</button>
+                        <button className='btn btn-primary rounded-md px-5 py-2 text-xl'>
+                            <Link to="/signup">Register</Link>
+                        </button>
+                        <button className='btn btn-warning mx-3 rounded-md px-5 py-2 text-xl'>
+                            <Link to="/login">Login</Link>
+                        </button>
                     </div>
                 </div>
 
